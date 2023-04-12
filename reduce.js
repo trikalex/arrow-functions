8a63a3dd0a45aa24ba2a2e03aac590b65ea78621
Alexandru Ursu <bodhipankh@yahoo.com>
2023-04-12 00:09:51 +0000

    Add reduce method exercises

--- reduce.js
+++ reduce.js
@@ -6,7 +6,20 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+// let sum = nums.reduce((acc, curr) => acc + curr);
 
+// let sum = nums.reduce((acc, curr) => {
+//     console.log(
+//     "Accumulator:", acc,
+//     "Current Value:", curr,
+//     "Total:", acc + curr
+//     );
+//     return acc + curr;
+// }, 10);
+
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
 const teamMembers = [
     {
